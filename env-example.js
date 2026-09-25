2d550ea66f7771e8ee0de81e471ad481a8ecbb97
Clever Tech Nexus <clevertechn@gmail.com>
2026-09-25 23:35:01 +0300

    Update env-example.js

--- env-example.js
+++ env-example.js
@@ -5,3 +5,17 @@ CHKzpfTNQ1Zr3WVimdVZhtZiB8FTriCkqRu
 tanzania-apps
 CLIENT_ID= IDujds6UHnkt9nQC0tZafnalN7336NYZ
 API_KEY= SKu2UxyWC0jKUx5kt1Q9Zh1DRQZsehsEVTTeXvFpUM
+
+
+
+NEXTAUTH_SECRET="jxDNNV6gjcWvSModTI+4JeWeWBx9OwFxTEGTlQBFOn0="
+NEXT_PUBLIC_APP_URL="https://app.clevertech.qzz.io"
+PLATFORM_COMMISSION_PERCENT="10"
+
+# Cloudflare R2 / S3-compatible object storage
+STORAGE_ENDPOINT="https://9d43cd37f0044aa5424bd164e343b991.r2.cloudflarestorage.com"
+STORAGE_BUCKET="tanzania-apps"
+STORAGE_ACCESS_KEY="35e30d81bd0d0fe50e22917fae57c935"
+STORAGE_SECRET_KEY="7fef346cb57ed01ff42e18687edeae2f5535a7fddd5130c45d15e0f91bafd277"
+STORAGE_PUBLIC_URL="https://files.clevertech.qzz.io"
+MAX_APP_UPLOAD_BYTES="1073741824"
